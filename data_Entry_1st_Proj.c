a86736cfd092dbe41394066da7410fb0bf6191c8
Mr.Sonsare <135420501+mrsonsare@users.noreply.github.com>
2023-07-06 09:15:43 +0530

    _Entry_1st_Proj.c

---
+++ data_Entry_1st_Proj.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+int a,b;
+
+printf("Enter Two Numbers\n");
+
+scanf("%d %d",&a,&b);
+
+printf("Addition of %d and %d is %d",a,b, (a+b));
+
+printf("\n");
+
+printf("Subtraction of %d and %d is %d",a,b,(a-b));
+
+return 0;
+
+}
