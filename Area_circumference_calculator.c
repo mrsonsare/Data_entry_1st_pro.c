8dfcb4f6ffebc2b95e6b5b250bae67af9cd51f02
Mr.Sonsare <135420501+mrsonsare@users.noreply.github.com>
2023-07-06 14:47:47 +0530

    Create Area_circumference_calculator.c

---
+++ Area_circumference_calculator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+
+float a, b, c;
+
+printf("Enter the value of Radius\n");
+
+scanf("%f", &a);
+
+b = a*a* 3.14;
+
+C = 2*3.14 *a;
+
+printf("The Value of Radius is %f\n", a);
+printf("Resultant Area Is %f \n and Circumference is %f", b,c);
+
+return 0;
+
+}
